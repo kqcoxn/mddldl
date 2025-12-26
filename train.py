4a39747eeca43a4beec1759198b396d23847935b
kqcoxn <mjy03730@163.com>
2025-12-26 23:16:03 +0800

    train-order

--- train.py
+++ train.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Load a model
-    model = YOLO("data/yolo11n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO(
+        "data/yolo11n.pt"
+    )  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="data/label.yaml", epochs=1000, batch=128)  # train the model
+    model.train(
+        data="datasets/face/marked/data.yaml", epochs=1000, batch=128
+    )  # train the model
